b46ea39ba4e52189c18f6b121787bdc031b40f8a
Kidus-19 <108111762+Kidus-y19@users.noreply.github.com>
2022-07-03 11:01:30 +0300

    oop concepts 3rd assignment

---
+++ C# Assignment 3/Shapes/Shapes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    abstract class Shape 
+    {
+        public abstract double calculateArea();
+        public abstract double calculatePerimeter();
+    }
+
+    class Rectangle : Shape
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+
+        public override double calculateArea()
+        {
+            return length * width;
+        }
+        public override double calculatePerimeter()
+        {
+            return 2 * (length + width);
+        }
+    }
+    class Circle : Shape
+    {
+        static readonly double PI = 3.14;
+        public double radius { get; set; }
+
+        public override double calculateArea()
+        {
+            return PI * radius * radius;
+        }
+        public override double calculatePerimeter()
+        {
+            return 2 * PI * radius;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Circle c1 = new Circle();
+            r1.length = 10;
+            r1.width = 2;
+            Console.WriteLine("Perimeterof the rectangle: " + r1.calculatePerimeter());
+            Console.WriteLine("Areaof the rectangle: " + r1.calculateArea());
+            c1.radius = 10;
+            Console.WriteLine("Perimeter of the circle: " + c1.calculatePerimeter());
+            Console.WriteLine("Area of the circle: " + c1.calculateArea());
+            Console.ReadKey(); 
+        }
+    }
+}
